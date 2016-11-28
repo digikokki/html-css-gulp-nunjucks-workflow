b93be04e71c01c8dea2d0814aaeb6c10544daead
Digikokki - Andreas <digikokki@gmail.com>
2016-11-28 14:18:49 +0200

    init

---
+++ Gulpfile.js
@@ -0,0 +1,99 @@
+// For development => gulp
+// For production  => gulp -p
+
+// Call Plugins
+var env         = require('minimist')(process.argv.slice(2)),
+    gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    plumber     = require('gulp-plumber'),
+    nunjucks    = require('gulp-nunjucks-html'),
+    minifyHtml  = require('gulp-minify-html'),
+    browserSync = require('browser-sync'),
+    uglify      = require('gulp-uglify'),
+    concat      = require('gulp-concat'),
+    gulpif      = require('gulp-if'),
+    cache       = require('gulp-cache'),
+    imagemin    = require('gulp-imagemin'),
+    sass 		= require('gulp-sass'),
+    cssnano 	= require('gulp-cssnano'),
+    sourcemaps 	= require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
+
+// Var
+var supported = [
+    'last 2 versions',
+    'safari >= 8',
+    'ie >= 10',
+    'ff >= 20',
+    'ios 6',
+    'android 4'
+];
+
+
+// Call Nunjucks for compile Templates
+gulp.task('nunjucks', function(){
+    return gulp.src('src/templates/*.html')
+        .pipe(plumber())
+        .pipe(nunjucks({
+            searchPaths: ['src/templates']
+        }))
+        .pipe(gulpif(env.p, minifyHtml()))
+        .pipe(gulp.dest('build/'));
+});
+
+// Call Uglify and Concat JS
+gulp.task('js', function(){
+    return gulp.src('src/js/**/*.js')
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(gulpif(env.p, uglify()))
+        .pipe(gulp.dest('build/js'));
+});
+
+// Call Sass
+gulp.task('sass', function(){
+        gulp.src(['src/sass/**/*.scss'])
+        .pipe(sass())
+        .pipe(cssnano({
+            autoprefixer: {browsers: supported, add: true}
+        }))
+        .pipe(gulp.dest('build/css'));
+});
+
+
+
+
+// Call Imagemin
+gulp.task('imagemin', function() {
+    return gulp.src('src/img/**/*')
+        .pipe(plumber())
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('build/img'));
+});
+
+// Call Watch
+gulp.task('watch', function(){
+    gulp.watch('src/templates/**/*.html', ['nunjucks']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+});
+
+
+gulp.task('browser-sync', function () {
+   var files = [
+      'build/**/*.html',
+      'build/css/**/*.css',
+      'build/img/**/*',
+      'build/js/**/*.js'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: './build/'
+      }
+   });
+});
+
+// Default task
+gulp.task('default', ['nunjucks', 'js', 'sass', 'imagemin', 'watch', 'browser-sync']);
