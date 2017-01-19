52f10b2d0d2a131aa70c61f26440a99227b30737
Digikokki Andreas <digikokki@gmail.com>
2017-01-19 10:32:36 +0200

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,26 @@
 // For production  => gulp -p
 
 // Call Plugins
-var env         = require('minimist')(process.argv.slice(2)),
-    gulp        = require('gulp'),
-    gutil       = require('gulp-util'),
-    plumber     = require('gulp-plumber'),
-    nunjucks    = require('gulp-nunjucks-html'),
-    minifyHtml  = require('gulp-minify-html'),
-    browserSync = require('browser-sync'),
-    uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat'),
-    gulpif      = require('gulp-if'),
-    cache       = require('gulp-cache'),
-    imagemin    = require('gulp-imagemin'),
-    sass 		= require('gulp-sass'),
-    cssnano 	= require('gulp-cssnano'),
-    sourcemaps 	= require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer');
-
+var env                 = require('minimist')(process.argv.slice(2)),
+    gulp                = require('gulp'),
+    gutil               = require('gulp-util'),
+    plumber             = require('gulp-plumber'),
+    nunjucks            = require('gulp-nunjucks-html'),
+    minifyHtml          = require('gulp-minify-html'),
+    browserSync         = require('browser-sync'),
+    uglify              = require('gulp-uglify'),
+    concat              = require('gulp-concat'),
+    gulpif              = require('gulp-if'),
+    cache               = require('gulp-cache'),
+    imagemin            = require('gulp-imagemin'),
+    sass 		            = require('gulp-sass'),
+    cssnano 	          = require('gulp-cssnano'),
+    sourcemaps 	        = require('gulp-sourcemaps'),
+    autoprefixer        = require('gulp-autoprefixer'),
+    customizeBootstrap  = require('gulp-customize-bootstrap'),
+    stylus              = require('gulp-stylus'),
+    inject              = require('gulp-inject'),
+    wiredep             = require('wiredep').stream;
 // Var
 var supported = [
     'last 2 versions',
@@ -29,51 +32,99 @@ var supported = [
     'android 4'
 ];
 
-
 // Call Nunjucks for compile Templates
 gulp.task('nunjucks', function(){
     return gulp.src('src/templates/*.html')
-        .pipe(plumber())
-        .pipe(nunjucks({
-            searchPaths: ['src/templates']
-        }))
-        .pipe(gulpif(env.p, minifyHtml()))
-        .pipe(gulp.dest('build/'));
+    .pipe(plumber())
+    .pipe(nunjucks({
+        searchPaths: ['src/templates']
+    }))
+    .pipe(wiredep({
+      ignorePath: 'build/'
+    }))
+    .pipe(gulpif(env.p, minifyHtml()))
+    .pipe(gulp.dest('build/'));
+});
+
+// Call inject links to html
+gulp.task('inject-links', ['nunjucks'], function(){
+  var target = gulp.src('./build/**/*.html');
+  target.pipe(inject(gulp.src(['./build/css/*.css','./build/js/*.js'],  {read: false}), {relative: true}))
+  .pipe(gulp.dest('build/'));
+});
+
+// Call inject to sass
+gulp.task('inject-sass', function(){
+  var injectAppFiles    = gulp.src('src/sass/bem/**/*.scss', {read: false});
+
+  var injectAppOptions = {
+    transform: transformFilepath,
+    starttag: '// inject:app',
+    endtag: '// endinject',
+    addRootSlash: false
+  };
+
+  var injectVendorFiles = gulp.src('src/sass/vendor/**/*.scss', {read: false});
+
+  var injectVendorOptions = {
+    transform: transformFilepath,
+    starttag: '// inject:vendor',
+    endtag: '// endinject',
+    addRootSlash: false
+  };
+
+  function transformFilepath(filepath) {
+   return '@import "' + filepath + '";';
+  }
+
+  return gulp.src('src/sass/main.scss')
+    .pipe(wiredep())
+    .pipe(inject(injectVendorFiles,injectVendorOptions))
+    .pipe(inject(injectAppFiles, injectAppOptions))
+    .pipe(gulp.dest('./src/sass'));
 });
 
 // Call Uglify and Concat JS
 gulp.task('js', function(){
     return gulp.src('src/js/**/*.js')
-        .pipe(plumber())
-        .pipe(concat('main.js'))
-        .pipe(gulpif(env.p, uglify()))
-        .pipe(gulp.dest('build/js'));
+    .pipe(plumber())
+    .pipe(concat('main.js'))
+    .pipe(gulpif(env.p, uglify()))
+    .pipe(gulp.dest('build/js'));
 });
 
 // Call Sass
 gulp.task('sass', function(){
-        gulp.src(['src/sass/**/*.scss'])
-        .pipe(sass())
-        .pipe(cssnano({
-            autoprefixer: {browsers: supported, add: true}
-        }))
-        .pipe(gulp.dest('build/css'));
+    gulp.src(['src/sass/**/*.scss'])
+    .pipe(plumber())
+    .pipe(sass({
+        outputStyle: 'compressed',
+		    includePaths: require('node-normalize-scss').includePaths
+    }))
+    .pipe(cssnano({
+        autoprefixer: {browsers: supported, add: true}
+    }))
+    .pipe(gulp.dest('build/css'));
 });
 
-
-
+// Get one .styl file and render
+gulp.task('styl', function () {
+  return gulp.src('src/styl/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('build/css'));
+});
 
 // Call Imagemin
 gulp.task('imagemin', function() {
     return gulp.src('src/img/**/*')
-        .pipe(plumber())
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('build/img'));
+    .pipe(plumber())
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('build/img'));
 });
 
 // Call Watch
 gulp.task('watch', function(){
-    gulp.watch('src/templates/**/*.html', ['nunjucks']);
+    gulp.watch('src/templates/**/*.html', ['nunjucks', 'inject-links']);
     gulp.watch('src/sass/**/*.scss', ['sass']);
     gulp.watch('src/js/**/*.js', ['js']);
     gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
@@ -88,12 +139,14 @@ gulp.task('browser-sync', function () {
       'build/js/**/*.js'
    ];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: './build/'
-      }
-   });
+   browserSync.init(files,{
+        server:{
+            baseDir: './build',
+            index: "front.html"
+        }
+    })
 });
 
 // Default task
-gulp.task('default', ['nunjucks', 'js', 'sass', 'imagemin', 'watch', 'browser-sync']);
+gulp.task('default', ['nunjucks', 'inject-links', 'inject-sass', 'sass', 'js', 'inject-sass', 'imagemin', 'watch', 'browser-sync'], function(){
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,82 @@
 console.log('it works!');
+
+var desktopOnly = window.matchMedia( "(min-width: 1200px)" );
+
+$(document).ready(function() {
+
+    // Toggle open sidebarWrapper
+    $('#brandLogo p').click(function() {
+
+      if ($(this).text() == $(this).data("text-swap")) {
+        $(this).text($(this).data("text-original"));
+      } else {
+        $(this).data("text-original", $(this).text());
+        $(this).text($(this).data("text-swap"));
+      }
+
+      $('#sidebarWrapper').toggleClass('open');
+      return true;
+    });
+
+    window.checkHasClass = function() {
+      if($('#sidebarWrapper').hasClass('open')) {
+        console.log('Kyllä');
+      } else {
+        console.log('Ei');
+      }
+    }
+
+    // Toggle open nav link on mobile size
+    $('#mobileNav').click(function() {
+      $('#sidebarContent').toggleClass('open');
+    });
+
+    /* JavaScript Media Queries */
+    if (matchMedia) {
+    	var mq = window.matchMedia("(min-width: 1200px)");
+    	mq.addListener(WidthChange);
+    	WidthChange(mq);
+    }
+
+    // media query change
+    function WidthChange(mq) {
+      if (mq.matches) {
+        console.log('window width is at least 1200px');
+        $('#postLinkWrapper').jScrollPane({
+          autoReinitialise: true
+        });
+      } else {
+        console.log('window width is less than 1200px');
+        window.onresize = function(){ location.reload(); }
+      }
+    }
+
+    // Decoline effect
+    (function() {
+			var lineMaker = new LineMaker({
+				parent: {element: document.querySelector(".pageHeader"), position: 'prepend'},
+        position: 'absolute',
+        lines: [
+          {top: '92%', left: 50, width: 54, height: 10, color: '#fff', hidden: true, content: 'hello', animation: { duration: 500, easing: 'easeInOutSine', delay: 400, direction: 'LeftRight' }},
+          {top: '92%', left: 100, width: 108, height: 10, color: '#05477c', hidden: true, content: 'hello', animation: { duration: 500, easing: 'easeInOutSine', delay: 550, direction: 'LeftRight' }},
+          {top: '92%', left: 150, width: 170, height: 10, color: '#065b9f', hidden: true, content: 'hello', animation: { duration: 500, easing: 'easeInOutSine', delay: 550, direction: 'LeftRight' }},
+        ]
+			});
+
+      setTimeout(function() {
+        lineMaker.animateLinesIn();
+    	}, 250);
+		})();
+
+    // Instafeed
+    var feed = new Instafeed({
+       get: 'user',
+       userId: '3711207612',
+       accessToken: '3711207612.2ad622d.e9c5bfe01e1445e4b9b8ac26768c2ea2',
+       clientId: '2ad622df35ab43b2b25e911c31868e44',
+       limit: 9,
+       template: '<a href="{{link}}" class="instafeedItem"><img src="{{image}}" /></a>'
+   });
+   feed.run();
+
+});
